edcc2d2fc2ce447230e275f8dfe0408dfe5d44b1
Sheryar  Ahmed Software Engineer <98022295+Sheryar-Ahmed@users.noreply.github.com>
2025-04-19 17:26:49 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>CI/CD pipelines with github actions</h1>
+        <h1>CI/CD pipelines with github actions now</h1>
       </header>
     </div>
   );
