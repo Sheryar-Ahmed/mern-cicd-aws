558aaf80f39b1f21de139ead35ce175504ebe461
Sheryar-Ahmed <royalsheryar505@gmail.com>
2025-04-19 17:15:28 +0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>CI/CD pipelines with github actions</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js Backend!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
